32dfab55978770d0eb45b0c332015ba76cc25f0e
zhzh25 <zhzh2592@mail.ru>
2024-05-27 14:45:34 +0300

    исправлены локаторы для поиска

--- locators/search/locators_MOL.py
+++ locators/search/locators_MOL.py
@@ -2,7 +2,7 @@ from selenium.webdriver.common.by import By
 
 
 class CoverageMapMol:
-    CHOOSE_BLSH_REGION = (By.XPATH, "//a[contains(text(), 'Балашиха (Московская область)')]")
+    CHOOSE_BLSH_REGION = (By.XPATH, "//a[contains(text(), 'Балашиха')]")
     CHOOSE_MSK_REGION = (By.XPATH, "//a[contains(text(), 'Москва')]")
     CHOOSE_THE_DISTRICT_BALASHIKHA = (By.XPATH, "(//a[contains(text(), 'Балашиха')])[1]")
     CHOOSE_THE_STREET_LENINA = (By.XPATH, "//a[contains(text(), 'Ленина пр-кт')]")
@@ -13,19 +13,7 @@ class CoverageMapMol:
     CHOOSE_THE_STREET_SHARIK = (By.XPATH, "//a[contains(text(), 'Шарикоподшипниковская ул')]")
     CHOOSE_THE_HOUSE_11 = (By.XPATH, "(//a[contains(text(), '11')])[2]")
     SCROLL = (By.XPATH, "//div[contains(text(), 'Отзывы о Москва Онлайн на Яндекс Картах')]")
-    TEXT_MOBILE = (By.XPATH, "//div[contains(text(), 'МОБИЛЬНЫЙ ИНТЕРНЕТ СО СКОРОСТЬЮ')]")
-    PANGINATION_2 = (By.XPATH, "//a[@href='/balashiha/doma-nzl/2']")
-    PANGINATION_3 = (By.XPATH, "//a[@href='/balashiha/doma-nzl/3']")
-    PANGINATION_4 = (By.XPATH, "//a[@href='/balashiha/doma-nzl/4']")
-    PANGINATION_5 = (By.XPATH, "//a[@href='/balashiha/doma-nzl/5']")
-    PANGINATION_6 = (By.XPATH, "//a[@href='/balashiha/doma-nzl/6']")
-    PANGINATION_7 = (By.XPATH, "//a[@href='/balashiha/doma-nzl/7']")
-    PANGINATION_2_MSK = (By.XPATH, "//a[@href='/address/%D0%BA%D0%BE%D0%BD%D1%8C%D0%BA%D0%BE%D0%B2%D0%BE-id1167/%D1%83%D0%BB-%D0%B1%D1%83%D1%82%D0%BB%D0%B5%D1%80%D0%BE%D0%B2%D0%B0-id265937/d-7-id129/2']")
-    PANGINATION_3_MSK = (By.XPATH, "//a[@href='/address/%D0%BA%D0%BE%D0%BD%D1%8C%D0%BA%D0%BE%D0%B2%D0%BE-id1167/%D1%83%D0%BB-%D0%B1%D1%83%D1%82%D0%BB%D0%B5%D1%80%D0%BE%D0%B2%D0%B0-id265937/d-7-id129/3']")
-    PANGINATION_4_MSK = (By.XPATH, "//a[@href='/address/%D0%BA%D0%BE%D0%BD%D1%8C%D0%BA%D0%BE%D0%B2%D0%BE-id1167/%D1%83%D0%BB-%D0%B1%D1%83%D1%82%D0%BB%D0%B5%D1%80%D0%BE%D0%B2%D0%B0-id265937/d-7-id129/4']")
-    PANGINATION_5_MSK = (By.XPATH, "//a[@href='/address/%D0%BA%D0%BE%D0%BD%D1%8C%D0%BA%D0%BE%D0%B2%D0%BE-id1167/%D1%83%D0%BB-%D0%B1%D1%83%D1%82%D0%BB%D0%B5%D1%80%D0%BE%D0%B2%D0%B0-id265937/d-7-id129/5']")
-    PANGINATION_6_MSK = (By.XPATH, "//a[@href='/address/%D0%BA%D0%BE%D0%BD%D1%8C%D0%BA%D0%BE%D0%B2%D0%BE-id1167/%D1%83%D0%BB-%D0%B1%D1%83%D1%82%D0%BB%D0%B5%D1%80%D0%BE%D0%B2%D0%B0-id265937/d-7-id129/6']")
-    PANGINATION_7_MSK = (By.XPATH, "//a[@href='/address/%D0%BA%D0%BE%D0%BD%D1%8C%D0%BA%D0%BE%D0%B2%D0%BE-id1167/%D1%83%D0%BB-%D0%B1%D1%83%D1%82%D0%BB%D0%B5%D1%80%D0%BE%D0%B2%D0%B0-id265937/d-7-id129/7']")
+    TEXT_MOBILE = (By.XPATH, "//h2[contains(text(), 'Мобильный интернет со скоростью до 100 Мб/с на б-р Тестовом')]")
 
 
 
