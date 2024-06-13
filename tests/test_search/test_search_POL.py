0d9ff08b697a53937f89441d91e7a889062de5bb
EkaterinaMangileva <mangileva.ek@gmail.com>
2024-06-13 11:55:32 +0500

    Обновила настройки для кейс

--- tests/test_search/test_search_POL.py
+++ tests/test_search/test_search_POL.py
@@ -1,19 +1,22 @@
 from pages.search.pol_page import CheckPage404, CheckTheCoverageMapPol
 import allure
 import time
+from qaseio.pytest import qase
 
-
-# from qaseio.pytest import qase
 
 @allure.suite("Тесты поиск на ПОЛ")
 class TestSearch:
-    def test_nonexistent_address_mol(self, driver):
+    @allure.title("Проверка отображения страницы 404 при запросе несуществующего URL")
+    @qase.title("Проверка отображения страницы 404 при запросе несуществующего URL")
+    # @qase.id(454)
+    def test_nonexistent_address_pol(self, driver):
         search_page = CheckPage404(driver, "https://piter-online.net/")
         search_page.open()
         search_page.check_nonexistent_address_pol()
 
     @allure.title("Проверка поиска в Колпино")
-    # @qase.title("Проверка поиска в Колпино")
+    @qase.title("Проверка поиска в Колпино")
+    @qase.id(457)
     def test_map_kolpino(self, driver):
         search_page = CheckTheCoverageMapPol(driver, "https://piter-online.net/")
         search_page.open()
@@ -24,7 +27,8 @@ class TestSearch:
         search_page.pangination_anisimova()
 
     @allure.title("Проверка карты покрытия в Лен. обл.")
-    # @qase.title("Проверка карты покрытия в Лен. обл.")
+    @qase.title("Проверка карты покрытия в Лен. обл.")
+    @qase.id(458)
     def test_map_len_obl(self, driver):
         search_page = CheckTheCoverageMapPol(driver, "https://piter-online.net/")
         search_page.open()
