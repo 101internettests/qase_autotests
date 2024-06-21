7af09e93abc3177941cc7cc450f029a9c7c93f77
EkaterinaMangileva <mangileva.ek@gmail.com>
2024-06-21 11:17:14 +0500

    Пробный тест для напоминалки

--- tests/test_hello_2/conftest.py
+++ tests/test_hello_2/conftest.py
@@ -1,7 +1,7 @@
 import pytest
-from config import bot, сhat_daily
+from config import bot, daily
 
 
 @pytest.hookimpl(trylast=True)
 def pytest_sessionfinish(session, exitstatus):
-    bot.send_message(сhat_daily, "Пора на дэйли https://us06web.zoom.us/j/89233765337?pwd=akr7biplJEpGIIeWxbHbc29W3yMg13.1 (скоро здесь будет картинка кота)")
+    bot.send_message(daily, "Пора на дэйли https://us06web.zoom.us/j/89233765337?pwd=akr7biplJEpGIIeWxbHbc29W3yMg13.1 (скоро здесь будет картинка кота)")
