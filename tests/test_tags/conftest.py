07c33a6ac6dfb3c7519ffde07a769abc6c267239
101internettests <160827243+101internettests@users.noreply.github.com>
2024-10-06 19:19:59 +0500

    Update conftest.py

--- tests/test_tags/conftest.py
+++ tests/test_tags/conftest.py
@@ -4,4 +4,4 @@ from config import bot, chat_id
 
 @pytest.hookimpl(trylast=True)
 def pytest_sessionfinish(session, exitstatus):
-    bot.send_message(chat_id, "Теговые тесты сделал, отчет по ссылке - https://101internettests.github.io/autotests/")
+    bot.send_message(chat_id, "Теговые тесты сделал, отчет по ссылке - https://101internettests.github.io/qase_autotests/")
